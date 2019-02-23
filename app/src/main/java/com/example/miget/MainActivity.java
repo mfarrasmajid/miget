6f3df83be68a3605be94c2e2b8fd9a6363cea82d
mfarrasmajid <m.farras.majid@gmail.com>
2019-02-23 10:49:36 +0700

    Second Sign Up

--- app/src/main/java/com/example/miget/MainActivity.java
+++ app/src/main/java/com/example/miget/MainActivity.java
@@ -1,21 +1,7 @@
 package com.example.miget;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextPaint;
-import android.text.method.HideReturnsTransformationMethod;
-import android.text.method.LinkMovementMethod;
-import android.text.method.PasswordTransformationMethod;
-import android.text.style.ClickableSpan;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,44 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // LINK TO SIGN UP
-        SpannableString ss = new SpannableString("Don't have an account? Sign Up");
-        ClickableSpan clickableSpan = new ClickableSpan() {
-            @Override
-            public void onClick(View textView) {
-                startActivity(new Intent(MainActivity.this, SignUp.class));
-            }
-            @Override
-            public void updateDrawState(TextPaint ds) {
-                super.updateDrawState(ds);
-                ds.setUnderlineText(false);
-            }
-        };
-        ss.setSpan(clickableSpan, 22, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-        TextView textView = (TextView) findViewById(R.id.signup);
-        textView.setText(ss);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
-        textView.setHighlightColor(Color.TRANSPARENT);
-
-        // SHOW PASSWORD
-        CheckBox checkbox = (CheckBox) findViewById(R.id.showpassword);
-        final EditText edtPassword = (EditText) findViewById(R.id.password);
-
-        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (isChecked) {
-                    // show password
-                    edtPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                } else {
-                    // hide password
-                    edtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                }
-            }
-        });
     }
-
-
 }
